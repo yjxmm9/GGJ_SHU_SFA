2a462ef10e0c7874273f2f92812553b92c57536a
yjxmm9 <qingliuyang0618@163.com>
2023-02-02 15:45:12 +0800

    氧气

---
+++ Src/Client/Assets/Scripts/Oxygen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oxygen : MonoBehaviour
+{
+    public float oxygenAmount;
+    public Collider myCollider;
+    public MeshRenderer myRenderer;
+    public AnimationCurve oxygenCurve;
+
+    public float lerpSpeed;
+    public Vector3 targetPos;
+    public float moveDis;
+
+    private void OnEnable()
+    {
+        oxygenAmount = 5;
+        targetPos = transform.position + Vector3.down * moveDis;
+    }
+    public void Breath()
+    {
+        myCollider.enabled = false;
+        StartCoroutine(BreathRoutine());
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed);
+    }
+
+    IEnumerator BreathRoutine()
+    {
+        Material oxygenMaterial = myRenderer.material;
+        float oxygenContent = 1;
+        while (oxygenContent > 0)
+        {
+            oxygenMaterial.color = new Color(oxygenMaterial.color.r, oxygenMaterial.color.g, oxygenMaterial.color.b, oxygenCurve.Evaluate(oxygenContent));
+            oxygenContent -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+}
